8140f6c51ed0bebd708aa69709fdf61f5f1bc6d7
Danilo Portillo <daniloportillo200030@gmail.com>
2020-04-18 02:02:15 -0600

    Interfaz de login terminada

--- Punto_de_venta/Login.Designer.cs
+++ Punto_de_venta/Login.Designer.cs
@@ -29,19 +29,73 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.txt1 = new System.Windows.Forms.Label();
+            this.user = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.Label();
+            this.log = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            resources.ApplyResources(this.txt1, "txt1");
+            this.txt1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txt1.Name = "txt1";
+            // 
+            // user
+            // 
+            resources.ApplyResources(this.user, "user");
+            this.user.Name = "user";
+            // 
+            // txt2
+            // 
+            resources.ApplyResources(this.txt2, "txt2");
+            this.txt2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.txt2.Name = "txt2";
+            // 
+            // password
+            // 
+            resources.ApplyResources(this.password, "password");
+            this.password.Name = "password";
+            // 
+            // txt3
+            // 
+            resources.ApplyResources(this.txt3, "txt3");
+            this.txt3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.txt3.Name = "txt3";
+            // 
+            // log
+            // 
+            resources.ApplyResources(this.log, "log");
+            this.log.Name = "log";
+            this.log.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(141)))));
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.txt1);
             this.Name = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label txt1;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.Label txt2;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label txt3;
+        private System.Windows.Forms.Button log;
     }
 }
 
