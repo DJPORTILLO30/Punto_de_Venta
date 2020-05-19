4be5b8cd09126f413a514a78e8c44cbb003566fa
CarlosVasquezDB <charlesvas16@outlook.com>
2020-05-18 21:39:07 -0600

    login reestructurado, con opacidad y efec to placeholder

--- Punto_de_venta/Login.Designer.cs
+++ Punto_de_venta/Login.Designer.cs
@@ -30,23 +30,30 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.txt1 = new System.Windows.Forms.Label();
-            this.user = new System.Windows.Forms.TextBox();
+            this.txt_user = new System.Windows.Forms.TextBox();
             this.txt2 = new System.Windows.Forms.Label();
-            this.password = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
             this.txt3 = new System.Windows.Forms.Label();
-            this.log = new System.Windows.Forms.Button();
+            this.btn_log = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txt1
             // 
             resources.ApplyResources(this.txt1, "txt1");
+            this.txt1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txt1.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.txt1.Name = "txt1";
             // 
-            // user
+            // txt_user
             // 
-            resources.ApplyResources(this.user, "user");
-            this.user.Name = "user";
+            resources.ApplyResources(this.txt_user, "txt_user");
+            this.txt_user.ForeColor = System.Drawing.Color.DimGray;
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Enter += new System.EventHandler(this.user_Enter);
+            this.txt_user.Leave += new System.EventHandler(this.txt_user_Leave);
             // 
             // txt2
             // 
@@ -54,10 +61,13 @@
             this.txt2.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.txt2.Name = "txt2";
             // 
-            // password
+            // txt_pass
             // 
-            resources.ApplyResources(this.password, "password");
-            this.password.Name = "password";
+            resources.ApplyResources(this.txt_pass, "txt_pass");
+            this.txt_pass.ForeColor = System.Drawing.Color.DimGray;
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Enter += new System.EventHandler(this.txt_pass_Enter);
+            this.txt_pass.Leave += new System.EventHandler(this.txt_pass_Leave);
             // 
             // txt3
             // 
@@ -65,24 +75,45 @@
             this.txt3.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.txt3.Name = "txt3";
             // 
-            // log
+            // btn_log
             // 
-            resources.ApplyResources(this.log, "log");
-            this.log.Name = "log";
-            this.log.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btn_log, "btn_log");
+            this.btn_log.Name = "btn_log";
+            this.btn_log.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(24)))), ((int)(((byte)(47)))));
+            this.panel1.Controls.Add(this.txt1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Login
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(141)))));
-            this.Controls.Add(this.log);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_log);
             this.Controls.Add(this.txt3);
-            this.Controls.Add(this.password);
+            this.Controls.Add(this.txt_pass);
             this.Controls.Add(this.txt2);
-            this.Controls.Add(this.user);
-            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txt_user);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
+            this.Opacity = 0.9D;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Login_MouseDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,11 +122,13 @@
         #endregion
 
         private System.Windows.Forms.Label txt1;
-        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox txt_user;
         private System.Windows.Forms.Label txt2;
-        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox txt_pass;
         private System.Windows.Forms.Label txt3;
-        private System.Windows.Forms.Button log;
+        private System.Windows.Forms.Button btn_log;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Punto_de_venta/Login.cs
+++ Punto_de_venta/Login.cs
@@ -7,14 +7,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Punto_de_venta
 {
     public partial class Login : Form
     {
+
         public Login()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void user_Enter(object sender, EventArgs e)
+        {
+            if (txt_user.Text == "Usuario")
+            {
+                txt_user.Text = "";
+                txt_user.ForeColor = Color.Black;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txt_user_Leave(object sender, EventArgs e)
+        {
+            if (txt_user.Text == "")
+            {
+                txt_user.Text = "Usuario";
+                txt_user.ForeColor = Color.DimGray;
+
+            }
+        }
+
+        private void txt_pass_Enter(object sender, EventArgs e)
+        {
+            if(txt_pass.Text == "Password")
+            {
+                txt_pass.Text = "";
+                txt_pass.ForeColor = Color.Black;
+                txt_pass.PasswordChar = '*';
+            }
+        }
+
+        private void txt_pass_Leave(object sender, EventArgs e)
+        {
+            if (txt_pass.Text == "")
+            {
+                txt_pass.Text = "Password";
+            }
+        }
+
+        private void Login_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
